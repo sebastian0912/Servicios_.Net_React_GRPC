7d0c404f87661eda0c35b8be04b2cff543d00d27
sebastian0912 <sebstiangc555@gmail.com>
2024-03-17 20:13:13 -0500

    Crear y editar listo

--- EmpleadosBackend/obj/Debug/net8.0/EmpleadosBackend.AssemblyInfo.cs
+++ EmpleadosBackend/obj/Debug/net8.0/EmpleadosBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpleadosBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141b9a7f5d8728e863a177ad676d114adb5b5df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpleadosBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpleadosBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
